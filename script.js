17400138f323401649ffce7fde60d8aecd9373c1
LYNX <frankez478@gmail.com>
2023-05-11 09:12:34 +0000

    Made a lot of changes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const calc = (h, m) => {
-    return h / m * m;
-}
+
+
+const calc = () => {
+    var fst = myform.fst.value;
+    var snd = myform.snd.value;
+    result = fst / snd / snd;
+    // myform.result.value = result;  
+    if (result <= 18.5) {
+        myform.result.value = `${result} -Underweight`;
+    } else if (result => 18.5 && result <= 24.5) {
+        myform.result.value = `${result} - Healthy`;
+    }
+    else {
+        myform.result.value = "Obese"
+    }
+};
