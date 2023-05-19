4196487b6ec1c48d6214ca249b8ee2b55f0c80b3
LYNX <frankez478@gmail.com>
2023-05-19 14:25:38 +0000

    Made Huge changes to the script and stylesheet

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+// function sum() {
 
+//     var total = Number(fst) + Number(snd);
+//     document.getElementById('ass').innerHTML = `The total is ${total}`;
+// }
 
 const calc = () => {
-    var fst = myform.fst.value;
-    var snd = myform.snd.value;
-    result = Math.floor(fst / snd / snd);
-    // myform.result.value = result;  
-    if (result <= 18.5) {
-        myform.result.value = `${result} - Underweight`;
-    } else if (result => 18.5 && result <= 24.5) {
-        myform.result.value = `${result} - Healthy`;
-    }
-    else {
-        myform.result.value = "Obese"
-    }
+  var fst = document.getElementById("fst").value;
+  var snd = document.getElementById("snd").value;
+  result = Math.floor(fst / snd / snd);
+  let ass = document.getElementById("ass");
+  // myform.result.value = result;
+  if (result <= 18.5) {
+    ass.innerHTML = `Your BMI is ${result} - Underweight`;
+  } else if ((result) => 18.5 && result <= 24.5) {
+    ass.innerHTML = `Your BMI is ${result} - Healthy`;
+  } else {
+    ass.innerHTML = `Your BMI is ${result} - Obese`;
+  }
 };
+
+// //16 ounces = 1 pound
+//1 pound = 0.454 kilogram
+//12 inches = 1 foot
